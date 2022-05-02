fdd67efd4f9ae5babb8d19ace163368c665b2c06
linn-khant <linkhantnyinyi242@gmail.com>
2022-05-01 22:37:31 -0700

    fetch data from api

---
+++ scripts/fetch.js
@@ -0,0 +1,25 @@
+const getRep = async () => {
+  const responce = await fetch(
+    "https://api.spoonacular.com/recipes/654959/information?apiKey=45055b7797714f42a6496662cb00415d&includeNutrition=true"
+  );
+  const data = await responce.json();
+  return data;
+};
+
+const getId = async () => {
+  const responce = await fetch(
+    "https://api.spoonacular.com/recipes/complexSearch?apiKey=45055b7797714f42a6496662cb00415d&query=pasta"
+  );
+  const data = await responce.json();
+  return data;
+};
+
+getId().then((data) => {
+  console.log(data.results);
+});
+
+getRep().then((data) => {
+  console.log(data);
+});
+
+//"https://via.placeholder.com/400x300"
